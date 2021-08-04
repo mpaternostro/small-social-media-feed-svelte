91bfc4e7ceda65fba784c19743d00a9aafa5b560
Marcelo <marcelodpaternostro@gmail.com>
2021-08-04 19:54:24 -0300

    feat: add layout

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	// your script goes here
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li><a href="/">Posts</a></li>
+			<li><a href="/users">Users</a></li>
+			<li><a href="/notifications">Notifications</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		background-color: #00d1b2;
+	}
+
+	ul {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 8rem;
+		height: 3.25rem;
+	}
+
+	li:hover {
+		background-color: #00b89c;
+	}
+
+	a {
+		color: #fff;
+	}
+</style>
